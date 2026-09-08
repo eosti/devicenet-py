7cf556dcd19acda223fb5fdde7f9430b774d0d0b
Reid Sox-Harris <reid@soxharris.com>
2026-09-08 00:21:26 -0400

    fix: dtype conversions broken
    - double precision floats pack/unpack properly
    - time types return datetype objects

--- devicenet/dtypes.py
+++ devicenet/dtypes.py
@@ -1,6 +1,10 @@
+import numbers
 import struct
+from datetime import UTC, datetime, time, timedelta
 from enum import Enum
 from typing import Any, Self
+
+import pandas as pd
 
 
 class DeviceNetDatatype(Enum):
@@ -78,16 +82,20 @@ class DeviceNetDatatype(Enum):
                 return val
             case cls.DATE:
                 cls.assert_length(val, 2)
-                return int.from_bytes(val, byteorder="big", signed=False)
+                days = int.from_bytes(val, byteorder="big", signed=False)
+                return pd.Timestamp(1970, 1, 1, tzinfo=UTC) + pd.Timedelta(days=days)
             case cls.TIME_OF_DAY:
                 cls.assert_length(val, 4)
-                return int.from_bytes(val, byteorder="big", signed=False)
+                time_ms = int.from_bytes(val, byteorder="little", signed=False)
+                delta = timedelta(milliseconds=time_ms)
+                dt = datetime(1970, 1, 1, tzinfo=UTC) + delta
+                return dt.time()
             case cls.REAL:
                 cls.assert_length(val, 4)
-                return struct.unpack("<f", val)
+                return struct.unpack("<f", val)[0]
             case cls.LREAL:
                 cls.assert_length(val, 8)
-                return struct.unpack("<ff", val)
+                return struct.unpack("<d", val)[0]
             case cls.SINT:
                 cls.assert_length(val, 1)
                 return int.from_bytes(val, byteorder="little", signed=True)
@@ -114,28 +122,32 @@ class DeviceNetDatatype(Enum):
                 return int.from_bytes(val, byteorder="little", signed=False)
             case cls.TIME:
                 cls.assert_length(val, 4)
-                return int.from_bytes(val, byteorder="little", signed=True)
+                time_ms = int.from_bytes(val, byteorder="little", signed=True)
+                return pd.Timedelta(milliseconds=time_ms)
             case cls.STIME:
                 cls.assert_length(val, 8)
-                return int.from_bytes(val, byteorder="little", signed=False)
+                time_ns = int.from_bytes(val, byteorder="little", signed=False)
+                return pd.Timestamp(1970, 1, 1, tzinfo=UTC) + pd.Timedelta(nanoseconds=time_ns)
             case cls.UTIME:
                 cls.assert_length(val, 8)
-                return int.from_bytes(val, byteorder="little", signed=False)
+                time_us = int.from_bytes(val, byteorder="little", signed=False)
+                return pd.Timestamp(1970, 1, 1, tzinfo=UTC) + pd.Timedelta(microseconds=time_us)
             case cls.ITIME:
                 cls.assert_length(val, 2)
-                return int.from_bytes(val, byteorder="little", signed=True)
+                time_ms = int.from_bytes(val, byteorder="little", signed=True)
+                return pd.Timedelta(milliseconds=time_ms)
             case cls.FTIME:
                 cls.assert_length(val, 4)
-                return int.from_bytes(val, byteorder="little", signed=True)
+                time_us = int.from_bytes(val, byteorder="little", signed=True)
+                return pd.Timedelta(microseconds=time_us)
             case cls.LTIME:
                 cls.assert_length(val, 8)
-                return int.from_bytes(val, byteorder="little", signed=True)
+                time_us = int.from_bytes(val, byteorder="little", signed=True)
+                return pd.Timedelta(microseconds=time_us)
             case cls.NTIME:
                 cls.assert_length(val, 8)
-                return int.from_bytes(val, byteorder="little", signed=True)
-            case cls.NTIME:
-                cls.assert_length(val, 8)
-                return int.from_bytes(val, byteorder="little", signed=True)
+                time_ns = int.from_bytes(val, byteorder="little", signed=True)
+                return pd.Timedelta(nanoseconds=time_ns)
             case cls.BITSTRING:
                 return val
             case cls.OCTETSTRING:
@@ -158,7 +170,7 @@ class DeviceNetDatatype(Enum):
             case cls.STRING2:
                 count = cls.decode(val[0:2], cls.UINT)
                 cls.assert_length(val, count * 2 + 2)
-                return val[2:].decode(encoding="utf-16")
+                return val[2:].decode(encoding="utf-16-le")
             case cls.STRINGN:
                 raise NotImplementedError
             case cls.STRINGI:
@@ -191,17 +203,24 @@ class DeviceNetDatatype(Enum):
                 cls.assert_length(val, 8)
                 return val
             case cls.DATE:
-                cls.assert_type(val, int)
-                return val.to_bytes(2, byteorder="big", signed=False)
+                cls.assert_type(val, datetime)
+                day_delta = (val - pd.Timestamp(1970, 1, 1, tzinfo=UTC)).days
+                return day_delta.to_bytes(2, byteorder="big", signed=False)
             case cls.TIME_OF_DAY:
-                cls.assert_type(val, int)
-                return val.to_bytes(4, byteorder="big", signed=False)
+                cls.assert_type(val, time)
+                ms = int(
+                    val.hour * 3600000
+                    + val.minute * 60000
+                    + val.second * 1000
+                    + val.microsecond / 1000
+                )
+                return ms.to_bytes(4, byteorder="little", signed=False)
             case cls.REAL:
-                cls.assert_type(val, float)
+                cls.assert_type(val, numbers.Real)
                 return struct.pack("<f", val)
             case cls.LREAL:
-                cls.assert_type(val, float)
-                return struct.pack("<ff", val)
+                cls.assert_type(val, numbers.Real)
+                return struct.pack("<d", val)
             case cls.SINT:
                 cls.assert_type(val, int)
                 return val.to_bytes(1, byteorder="little", signed=True)
@@ -227,26 +246,35 @@ class DeviceNetDatatype(Enum):
                 cls.assert_type(val, int)
                 return val.to_bytes(8, byteorder="little", signed=False)
             case cls.TIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(4, byteorder="little", signed=True)
+                cls.assert_type(val, timedelta)
+                time_ms = int(val / pd.Timedelta(milliseconds=1))
+                return time_ms.to_bytes(4, byteorder="little", signed=True)
             case cls.STIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(8, byteorder="little", signed=True)
+                cls.assert_type(val, datetime)
+                delta_time = val - pd.Timestamp(1970, 1, 1, tzinfo=UTC)
+                time_ns = delta_time.value
+                return time_ns.to_bytes(8, byteorder="little", signed=True)
             case cls.UTIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(8, byteorder="little", signed=False)
+                cls.assert_type(val, datetime)
+                delta_time = val - pd.Timestamp(1970, 1, 1, tzinfo=UTC)
+                time_us = int(delta_time.value / 1e3)
+                return time_us.to_bytes(8, byteorder="little", signed=True)
             case cls.ITIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(2, byteorder="little", signed=True)
+                cls.assert_type(val, timedelta)
+                time_ms = int(val / timedelta(milliseconds=1))
+                return time_ms.to_bytes(2, byteorder="little", signed=True)
             case cls.FTIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(4, byteorder="little", signed=True)
+                cls.assert_type(val, timedelta)
+                time_us = int(val / pd.Timedelta(microseconds=1))
+                return time_us.to_bytes(4, byteorder="little", signed=True)
             case cls.LTIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(8, byteorder="little", signed=True)
+                cls.assert_type(val, timedelta)
+                time_us = int(val / pd.Timedelta(microseconds=1))
+                return time_us.to_bytes(8, byteorder="little", signed=True)
             case cls.NTIME:
-                cls.assert_type(val, int)
-                return val.to_bytes(8, byteorder="little", signed=True)
+                cls.assert_type(val, timedelta)
+                time_ns = int(val / pd.Timedelta(nanoseconds=1))
+                return time_ns.to_bytes(8, byteorder="little", signed=True)
             case cls.BITSTRING:
                 cls.assert_type(val, bytes)
                 return val
@@ -258,7 +286,7 @@ class DeviceNetDatatype(Enum):
             case cls.ETH_MAC_ADDR:
                 cls.assert_type(val, list)
                 cls.assert_length(val, 6)
-                return bytes([val.to_bytes(1, signed=False) for b in val])
+                return b"".join([b.to_bytes(1, signed=False) for b in val])
             case cls.DATE_AND_TIME:
                 raise NotImplementedError
             case cls.SHORT_STRING:
@@ -275,7 +303,7 @@ class DeviceNetDatatype(Enum):
                 cls.assert_type(val, str)
                 count = len(val)
                 return count.to_bytes(2, byteorder="little", signed=False) + val.encode(
-                    encoding="utf-16"
+                    encoding="utf-16-le"
                 )
             case cls.STRINGN:
                 raise NotImplementedError
